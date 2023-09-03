7dc59f5d6fb5b271d31c14439d0537b202388f96
Astrid <gonzalezvargasastrid@gmail.com>
2023-09-03 12:00:35 -0300

    Actualización de paginas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { CursosPage } from './cursos/cursos.page';
+import { AsistenciaPage } from './asistencia/asistencia.page';
 
 const routes: Routes = [
   {
@@ -14,11 +16,24 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-  },  {
+  },
+  {
     path: 'menuprof',
     loadChildren: () => import('./menuprof/menuprof.module').then( m => m.MenuprofPageModule)
   },
 
+  {
+    path: 'cursos',
+    loadChildren: () => import('./cursos/cursos.module').then( m => m.CursosPageModule)
+  },
+
+    {
+    path: 'asistencia',
+    loadChildren: () => import('./asistencia/asistencia.module').then( m => m.AsistenciaPageModule)
+  },
+
+  { path: 'cursos', component: CursosPage },
+  { path: 'asitencia', component: AsistenciaPage }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,10 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { MenuModule } from './menu/menu.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,MenuModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
