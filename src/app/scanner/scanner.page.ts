e3626875406505533c5209473078685710a717d0
confloresc <con.floresc@duocuc.cl>
2023-09-08 01:04:33 -0300

    boton alarma exito ok

--- src/app/scanner/scanner.page.ts
+++ src/app/scanner/scanner.page.ts
@@ -1,21 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+// scanner.page.ts
+import { Component } from '@angular/core';
 import { AlertController, NavController } from '@ionic/angular';
-
 
 @Component({
   selector: 'app-scanner',
   templateUrl: './scanner.page.html',
   styleUrls: ['./scanner.page.scss'],
 })
-
-export class ScannerPage implements OnInit {
-  alertButtons: string[] = [];
+export class ScannerPage {
 
   constructor(private alertController: AlertController, private navCtrl: NavController) { }
 
   async presentAlert() {
     const alert = await this.alertController.create({
-
+      header: 'Éxito',
+      message: 'La operación se completó con éxito.',
+      backdropDismiss: false, // Evita que la alarma se cierre haciendo clic fuera de ella
+      buttons: [
+        {
+          text: 'OK',
+          handler: () => {
+            this.goToLoginPage(); // Redirige a la página de inicio de sesión al hacer clic en OK
+          }
+        }
+      ]
     });
 
     await alert.present();
@@ -25,10 +33,4 @@ export class ScannerPage implements OnInit {
     // Utiliza NavController para navegar a la página de login
     this.navCtrl.navigateForward('/login'); // Asegúrate de que '/login' sea la ruta correcta a tu página de login
   }
-
-  ngOnInit() {
-    // Código relacionado con ngOnInit
-  }
 }
-
-
