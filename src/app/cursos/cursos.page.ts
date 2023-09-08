b290e5fc7a3edfde7768597a98bb5c840d2ca980
Astrid <gonzalezvargasastrid@gmail.com>
2023-09-07 21:12:01 -0300

    actualización

--- src/app/cursos/cursos.page.ts
+++ src/app/cursos/cursos.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-cursos',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursosPage implements OnInit {
 
-  constructor() { }
+  constructor(private navCtrl: NavController) {} // Inyecta NavController en el constructor
+
+  goToLoginPage() {
+    // Utiliza NavController para navegar a la página de login
+    this.navCtrl.navigateForward('/menuprof'); // Asegúrate de que '/login' sea la ruta correcta a tu página de login
+  }
 
   ngOnInit() {
   }
