f0ca1c39a7641f29bc2d71bb752f61248493cdef
Astrid <ast.gonzalez@duocuc.cl>
2023-11-01 19:24:54 -0300

    CURSOS ACTUALIZADO

--- src/app/prof-registro-asistencia/prof-registro-asistencia.page.spec.ts
+++ src/app/prof-registro-asistencia/prof-registro-asistencia.page.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 import { ProfRegistroAsistenciaPage } from './prof-registro-asistencia.page';
 
 describe('ProfRegistroAsistenciaPage', () => {
