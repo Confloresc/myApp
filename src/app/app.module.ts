99c21b655a749f9a6715372311d7f4bfa4c6e523
Astrid <ast.gonzalez@duocuc.cl>
2023-11-14 21:22:14 -0300

    quitar sqlite

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
-import {SQLite} from "@awesome-cordova-plugins/sqlite/ngx"
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AuthGuard } from 'src/app/services/auth-guard.service';
 
@@ -19,7 +18,7 @@ import { AuthenticationService } from '../app/services/authentication.service';
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot(),HttpClientModule],
-  providers: [AuthenticationService,AuthGuard,SQLite,{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [AuthenticationService,AuthGuard,{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
