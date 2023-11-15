b28cbbe10d278db5620d864c58026dc8f09a0783
Astrid <ast.gonzalez@duocuc.cl>
2023-11-14 21:38:06 -0300

    agrergar android

--- android/app/src/main/assets/public/1659.9c20b00c9a869dd7.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkapp=self.webpackChunkapp||[]).push([[1659],{1659:(e,n,o)=>{o.r(n),o.d(n,{PageNotFoundPageModule:()=>t});var _=o(6814),g=o(95),d=o(7027),l=o(6864),u=o(6689);let t=(()=>{var a;class P{}return(a=P).\u0275fac=function(M){return new(M||a)},a.\u0275mod=u.oAB({type:a}),a.\u0275inj=u.cJS({imports:[_.ez,g.u5,d.Pc,l.PageNotFoundPageRoutingModule]}),P})()}}]);
