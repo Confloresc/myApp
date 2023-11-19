fafea3739baec2e741e0c5a4715209daf6a73ee6
Astrid <ast.gonzalez@duocuc.cl>
2023-11-19 14:37:11 -0300

    se recupera y se actualiza con un port

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -9,7 +9,7 @@ import { catchError, map } from 'rxjs/operators';
 export class AuthenticationService {
   constructor(private http: HttpClient) {}
 
-  private apiUrl = 'http://localhost:8000';
+  private apiUrl = 'https://24tpg1hl-8000.brs.devtunnels.ms/';
   private userEmail: string | undefined;
 
   setUserEmail(email: string) {
@@ -17,7 +17,7 @@ export class AuthenticationService {
   }
 
   login(email: string, password: string): Observable<any> {
-    return this.http.post<any>(`${this.apiUrl}/login`, { email, password });
+    return this.http.post<any>(`${this.apiUrl}login`, { email, password }, { withCredentials: true });
   }
 
   // Método para cerrar sesión
@@ -27,18 +27,37 @@ export class AuthenticationService {
 
   isAuthenticatedUser(): Observable<boolean> {
     if (this.userEmail) {
-      return this.http.get<{ isAuthenticated: boolean }>(`${this.apiUrl}/authenticate?email=${this.userEmail}`).pipe(
-        map((response) => response.isAuthenticated),
-        catchError((error) => of(false))
+      const encodedEmail = encodeURIComponent(this.userEmail);
+      return this.http.get<{ user_type: string }>(`${this.apiUrl}authenticate?email=${encodedEmail}`, { withCredentials: true }).pipe(
+        map((response) => {
+          this.handleUserType(response.user_type);
+          return true;
+        }),
+        catchError((error) => {
+          this.handleUserType(null);
+          return of(false);
+        })
       );
     } else {
-      return of(false); // El usuario no ha iniciado sesión, por lo tanto, no está autenticado.
+      return of(false);
+    }
+  }
+
+  private handleUserType(userType: string | null) {
+    // Implementa la lógica para manejar el tipo de usuario (por ejemplo, redirigir a diferentes rutas).
+    // Aquí puedes actualizar la lógica según tus necesidades.
+    if (userType === 'profesor') {
+      // Redirige a la ruta del profesor
+    } else if (userType === 'alumno') {
+      // Redirige a la ruta del alumno
+    } else {
+      // Maneja otros casos si es necesario
     }
   }
 
   // Define el método get_user_info que recibe el correo electrónico como argumento
   get_user_info(email: string): Observable<any> {
-    const url = `${this.apiUrl}/users/${encodeURIComponent(email)}`;
+    const url = `${this.apiUrl}users/${encodeURIComponent(email)}`;
     return this.http.get(url);
   }
 }
