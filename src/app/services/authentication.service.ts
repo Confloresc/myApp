728f2c29b8b0e1ea2600f84c650e0a278e034244
Astrid <ast.gonzalez@duocuc.cl>
2023-11-28 20:16:26 -0300

    api mocha

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 
@@ -9,7 +9,7 @@ import { catchError, map } from 'rxjs/operators';
 export class AuthenticationService {
   constructor(private http: HttpClient) {}
 
-  private apiUrl = 'http://127.0.0.1:8000';
+  private apiUrl = 'https://apimocha.com/asistencia';
   private userEmail: string | undefined;
 
   setUserEmail(email: string) {
@@ -17,8 +17,18 @@ export class AuthenticationService {
   }
 
   login(email: string, password: string): Observable<any> {
-    return this.http.post<any>(`${this.apiUrl}/login`, { email, password }, { withCredentials: true });
+    return this.http.post<any>(`${this.apiUrl}/users`, { email, password })
+      .pipe(
+        catchError((error: any) => {
+          console.error('Error during login:', error);
+          if (error instanceof HttpErrorResponse && error.status === 200) {
+            console.error('Non-JSON response:', error.error);
+          }
+          return of(null);
+        })
+      );
   }
+
 
   // Método para cerrar sesión
   logout() {
@@ -27,36 +37,18 @@ export class AuthenticationService {
 
   isAuthenticatedUser(): Observable<boolean> {
     if (this.userEmail) {
-      return this.http.get<{ user_type: string }>(`${this.apiUrl}/authenticate?email=${this.userEmail}`).pipe(
-        map((response) => {
-          this.handleUserType(response.user_type);
-          return true; // Indica que la autenticación fue exitosa
-        }),
-        catchError((error) => {
-          this.handleUserType(null); // Restablece el tipo de usuario en caso de error
-          return of(false);
-        })
+      return this.http.get<{ isAuthenticated: boolean }>(`${this.apiUrl}/users/?email=${this.userEmail}`).pipe(
+        map((response) => response.isAuthenticated),
+        catchError((error) => of(false))
       );
     } else {
       return of(false); // El usuario no ha iniciado sesión, por lo tanto, no está autenticado.
     }
   }
 
-  private handleUserType(userType: string | null) {
-    // Implementa la lógica para manejar el tipo de usuario (por ejemplo, redirigir a diferentes rutas).
-    // Aquí puedes actualizar la lógica según tus necesidades.
-    if (userType === 'profesor') {
-      // Redirige a la ruta del profesor
-    } else if (userType === 'alumno') {
-      // Redirige a la ruta del alumno
-    } else {
-      // Maneja otros casos si es necesario
-    }
-  }
-
   // Define el método get_user_info que recibe el correo electrónico como argumento
   get_user_info(email: string): Observable<any> {
-    const url = `${this.apiUrl}/users/${encodeURIComponent(email)}`;
+    const url = `${this.apiUrl}users/${encodeURIComponent(email)}`;
     return this.http.get(url);
   }
 }
