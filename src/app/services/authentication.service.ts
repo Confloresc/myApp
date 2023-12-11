2a7e0bdad7e3d622c6c90bf18be9950acedb55f1
AstGon <127602477+AstGon@users.noreply.github.com>
2023-12-11 16:38:16 -0300

    Update authentication.service.ts

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -9,7 +9,7 @@ import { catchError, map } from 'rxjs/operators';
 export class AuthenticationService {
   constructor(private http: HttpClient) {}
 
-  private apiUrl = 'http://13.51.201.225';
+  private apiUrl = 'http://127.0.0.1:8000';
   private userEmail: string | undefined;
 
   setUserEmail(email: string) {
